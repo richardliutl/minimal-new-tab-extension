9b5b78907242479f66289880c5f9165bc31cedeb
richardliutl <richardliutl@gmail.com>
2022-04-28 17:49:25 -0400

    Update sr.js

--- bookmarklets/sr.js
+++ bookmarklets/sr.js
@@ -1,8 +1,8 @@
 javascript:
-	u = location.href;
+  u = location.href;
 if (u.match('www.google.com')) {
-	query = u.match(/\?q\=(.*?)&/)[1]
-	window.location = "https://www.google.com/search?q=site%3Areddit.com+" + query;
+  query = u.match(/\?q\=(.*?)&/)[1]
+  window.location = "https://www.google.com/search?q=site%3Areddit.com+" + query;
 } else {
-	alert('Not a Google URL.');
+  alert('Not a Google URL.');
 }
