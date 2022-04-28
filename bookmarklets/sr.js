dd2c0e1d262087dff6954f59df18cc34350310af
richardliutl <richardliutl@gmail.com>
2022-04-28 17:50:09 -0400

    Update sr.js

--- bookmarklets/sr.js
+++ bookmarklets/sr.js
@@ -1,7 +1,7 @@
 javascript:
   u = location.href;
 if (u.match('www.google.com')) {
-  query = u.match(/\?q\=(.*?)&/)[1]
+  query = u.match(/\?q\=(.*?)&/)[1];
   window.location = "https://www.google.com/search?q=site%3Areddit.com+" + query;
 } else {
   alert('Not a Google URL.');
