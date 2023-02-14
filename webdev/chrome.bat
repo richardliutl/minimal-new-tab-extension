88c2c7e6879cef0222cbbdcb727a6beddc0c56f0
richardliutl <richardliutl@gmail.com>
2023-02-14 14:40:45 -0500

    Update chrome.bat

--- webdev/chrome.bat
+++ webdev/chrome.bat
@@ -1,1 +1,2 @@
-chrome.exe --user-data-dir="C:\Users\Richard\Documents\Chrome Dev Session" --disable-web-security
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\Users\Richard\Documents\Chrome Dev Session" --disable-web-security
+exit 0
